d6e2347c3b9f641ea1d75b36512ae17d52ef6219
Sarah Chow <31673054+sarahchoww@users.noreply.github.com>
2020-05-29 11:01:54 -0400

    Demo ver

--- Writing to shared memory/Semaphore/Header Files/main.cpp
+++ Writing to shared memory/Semaphore/Header Files/main.cpp
@@ -12,9 +12,6 @@ int main(int argc, char *argv[])
 {
 
     Transfer *process = nullptr;
-    std::clock_t start;
-    double duration;
-    int run;
 
     if (argc == 2)
     {
@@ -24,51 +21,24 @@ int main(int argc, char *argv[])
         if (inputType == "sender")
         {
             process = new Sender();
-
-            for(int i = 0; i < 5; i++)
-            {
-
-                duration = 0;
-                start = std::clock();
-                run = 0;
-
-                if ((run = process->run()) == 1)
-                {
-                    std::cout << "run failed\n";
-                    return(1);
-                }
-
-
-                while ((duration < 2) && (run != 1)) // Wait 2 seconds if process not been terminated
-                {
-                    duration = (std::clock() - start) / (double) CLOCKS_PER_SEC;
-                    // CLOCKS_PER_SEC is number of clock ticks per second (1 000 000 ticks per second)
-                    // Take current clock minus clock from start divided by ticks per second
-                } 
-
-            }
         }
         else if (inputType == "receiver")
         {
             process = new Receiver();
-
-            while(true)
-            {
-                if (process->run() == 1)
-                {
-                    std::cout << "run failed\n";
-                    return(1);
-                }
-            }
         }
         else
         {
             std::cout << "Invalid entry\n";
-            return(1);
+            return (1);
         }
-
-        delete process; // Destructor
     }
 
-    return(0);
+    if (process->run() == 1)
+    {
+        std::cout << "run failed\n";
+        return (1);
+    }
+
+    delete process; // Destructor
+    return (0);
 }
