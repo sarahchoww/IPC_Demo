758f3266065f34878f418851275ec25dacce1c95
Sarah Chow <31673054+sarahchoww@users.noreply.github.com>
2020-05-29 11:02:11 -0400

    demo ver

--- Writing to shared memory/Semaphore/Header Files/receiver.cpp
+++ Writing to shared memory/Semaphore/Header Files/receiver.cpp
@@ -8,108 +8,73 @@ Receiver::Receiver()
 
 Receiver::~Receiver()
 {
-    Transfer::cleanUp();
-    cleanUp();
+    Transfer::cleanUpMap();
 }
 
-
 int Receiver::setUp()
-{   
+{
     if ((addr = (struct memory_data *)mmap(NULL, sizeof(struct memory_data), PROT_READ, MAP_SHARED, fileDir, 0)) == MAP_FAILED)
     {
         std::cout << "mmap failed\n";
-        return(1);
+        return (1);
     }
 
-    return(0);
+    return (0);
 }
 
-
-int Receiver::run ()
+int Receiver::run()
 {
-    std::cout << "Waiting for data\n";
+    bool run = true;
+    struct timespec timer;
 
-
-
-    if ((sem_wait(semNewData)) == -1) // Wait for signal
+    while (run)
     {
-        std::cout << "sem wait new data failure\n" << errno <<std::endl;
-        return(1);
+        if (clock_gettime(CLOCK_REALTIME, &timer) == -1) // Get current time and store in timer
+        {
+            perror("clock_gettime");
+            exit(EXIT_FAILURE);
+        }
+
+        timer.tv_sec += 10; // Add 10 seconds to timer, when to time out
+
+        std::cout << "Waiting for data\n";
+
+        if ((sem_timedwait(semNewData, &timer)) == -1) // Wait for signal
+        {
+            if (errno == ETIMEDOUT) // Timed out
+            {
+                run = false;
+                std::cout << "Timed out\n";
+                cleanUpFiles(); // Delete files in shared memory
+            }
+            else // Error
+            {
+                std::cout << "sem wait new data failure\n"
+                          << errno << std::endl;
+                return (1);
+            }
+        }
+        else
+        {
+
+            // Reset the time
+            time(&my_time);              // Current time put into my_time
+            ltime = localtime(&my_time); // Return the 'struct tm' representation of timer in local time zone
+            outputTime = asctime(ltime); // Takes in a pointer, converts to string
+
+            std::cout << "Receiving\n";
+
+            display();
+
+            std::cout << "\n\n";
+
+            if ((sem_post(semReceived)) == -1) // Notify data has been received
+            {
+                std::cout << "Post notify failed\n";
+                return (1);
+            }
+        }
     }
 
-
-    // Reset the time
-    time(&my_time);                       // Current time put into my_time
-    ltime = localtime(&my_time); // Return the 'struct tm' representation of timer in local time zone
-    outputTime = asctime(ltime);   // Takes in a pointer, converts to string
-
-
-    std::cout << "Receiving\n";
-
-    display();
-
-    std::cout << "\n\n";
-
-    if ((sem_post(semReceived)) == -1) // Notify data has been received
-    {
-        std::cout << "Post notify failed\n";
-        return(1);
-    }
-
-
-    return(0);
-}
-
-void Receiver::cleanUp()
-{
-    shm_unlink(FILENAME);
-
-    sem_close(semNewData);
-    sem_close(semReceived);
-
-    remove("/dev/shm/sem.signal-new-data");
-    remove("/dev/shm/sem.wait-received");
-
-    
-    
-    /*std::string tempRandom = SEM_NEWDATA;
-    char deleteFile1[tempRandom.length()] = SEM_NEWDATA;
-    char *newCopy1 = nullptr;
-
-    memcpy(newCopy1, deleteFile1 + 1, strlen(deleteFile1)); // rid of the slash at start of name
-
-    std::string temp = FILEPATH;
-
-    char rm1[temp.length() + strlen(newCopy1)] = FILEPATH; // variable for file path to shared memory, size of both strings
-
-    strcat(rm1, newCopy1); // concatenate file path and file name into "rm"
-
-    std::cout << "\n"
-              << rm1 << std::endl;
-
-    if (remove(rm1) != 0)
-    {
-        std::cout << "remove error\n"
-                  << errno;
-    }
-
-    tempRandom = SEM_RECEIVED;
-    char deleteFile[tempRandom.length()] = SEM_RECEIVED;
-    char *newCopy = nullptr;
-
-    memcpy(newCopy, deleteFile + 1, strlen(deleteFile)); // rid of the slash at start of name
-
-    char rm[temp.length() + strlen(newCopy)] = FILEPATH; // variable for file path to shared memory, size of both strings
-
-    strcat(rm, newCopy); // concatenate file path and file name into "rm"
-
-    std::cout << "\n"
-              << rm << std::endl;
-
-    if (remove(rm) != 0)
-    {
-        std::cout << "remove error\n"
-                  << errno;
-
-    */
+    return (0);
 }
